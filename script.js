2c8aaa185ae3aeb54b1e700bb06bfd4e3ecd5795
codessbabe <oana.anghelachi@gmail.com>
2021-03-13 00:23:47 +0100

    Fetch initial posts from API

---
+++ script.js
@@ -0,0 +1,48 @@
+const postsContainer = document.getElementById("posts-container")
+const loading = document.querySelector(".loader")
+const filter = document.getElementById("filter")
+
+let limit = 3
+let page = 1
+
+// Fetch posts from API
+async function getPosts() {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&page=${page}`
+  )
+
+  // this res.json() returns a PROMISE
+  const data = await res.json()
+
+  // also returning a PROMISE
+  return data
+}
+
+
+// Show posts in DOM
+async function showPosts() { 
+  // this returns a PROMISE, so we want to await on that
+  const posts = await getPosts();
+
+  posts.forEach(post => { 
+    const postEl = document.createElement('div')
+    postEl.classList.add('post')
+    postEl.innerHTML = `
+    <div class="number">${post.id}</div>
+    <div class="post-info">
+      <h2 class="post-title">${post.title}</h2>
+      <p class="post-body">${post.body}</p>
+    </div>
+    `
+
+    postsContainer.appendChild(postEl)
+  })
+}
+
+// Show initial posts
+showPosts();
+
+
+
+
+
